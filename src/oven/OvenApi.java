db55ae76bae0d73cf321f183574b9f0f487462fb
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-01-07 13:31:25 +0100

    Add togglevSync and toggleWindowVisibility to API

--- src/oven/OvenApi.java
+++ src/oven/OvenApi.java
@@ -1,7 +1,22 @@
 package oven;
 
+import engine.Window;
+import static org.lwjgl.glfw.GLFW.*;
+
 public class OvenApi {
     public static void updateClientTitle(String newTitle){
         OvenOpenData.windowTitle = newTitle;
+    }
+    public static void togglevSync(boolean on){
+        Window.setVSync(on);
+    }
+    public static void toggleWindowVisibility(boolean on){
+        long windowHandle = Window.getWindowHandle();
+        if(on){
+            glfwShowWindow(windowHandle);
+        }
+        else{
+            glfwHideWindow(windowHandle);
+        }
     }
 }
